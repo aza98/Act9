462fd297eef327d84a7ca4f142e6604119d0e9ab
aza98 <Edgar.azael98@gmail.com>
2021-11-25 22:30:30 -0600

    Act 9 V 1.2
    arreglo el error que provocaba que no se podía visualizar correcto la actividad

---
+++ Act9/Shared/Act9App.swift
@@ -0,0 +1,17 @@
+//
+//  Act9App.swift
+//  Shared
+//
+//  Created by Azael Reyna on 20/11/21.
+//
+
+import SwiftUI
+
+@main
+struct Act9App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
